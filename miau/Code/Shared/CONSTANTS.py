63ba6013e2f9010713f8834b800dce801183e300
matheus-srego <matheus.gomes@al.infnet.edu.br>
2022-02-03 14:24:52 -0300

    Criando arquivo de constantes

---
+++ miau/Code/Shared/CONSTANTS.py
@@ -0,0 +1,56 @@
+# PATHS -------------------------------------------
+URL_CSV = 'https://drive.google.com/file/d/1if6xBaxKSaWAd5kvXT5SnppB1EvQTGO1/view?usp=sharing'
+RAW_CSV = '~/M.I.A.U./miau/Data/Raw/HomeC.csv'
+PROCESSED_CSV = '~/M.I.A.U./miau/Data/Processed/home.csv'
+# -------------------------------------------------
+
+# FORMATS -----------------------------------------
+FORMAT_DATE_EUA = '%Y-%m-%d %H:%M:%S'
+# -------------------------------------------------
+
+# COLUMNS PROCESSED CSV ---------------------------
+APPARENT_TEMPERATURE = 'apparent_temperature'
+KW_USE = 'KW_use'
+KW_GENERATION = 'KW_generation'
+KW_HOUSE = 'KW_house'
+KW_HOME_OFFICE = 'KW_home_office'
+KW_LIVING_ROOM = 'KW_living_room'
+KW_KITCHEN = 'KW_kitchen'
+KW_WINE_CELLAR = 'KW_wine_cellar'
+KW_GARAGE_DOOR = 'KW_garage_door'
+KW_BARN = 'KW_barn'
+# -------------------------------------------------
+
+# COLUMNS RAW CSV. --------------------------------
+COLUMNS_SOLAR = 'Solar [kW]'
+COLUMN_ICON = 'icon'
+COLUMN_HUMIDITY = 'humidity'
+COLUMN_VISIBILITY = 'visibility'
+COLUMN_SUMARY = 'summary'
+COLUMN_PRESSURE = 'pressure'
+COLUMN_WIND_SPEED = 'windSpeed'
+COLUMN_CLOUD_COVER = 'cloudCover'
+COLUMN_WIND_BEARING = 'windBearing'
+COLUMN_PRECIP_INTENSITY = 'precipIntensity'
+COLUMN_DEW_POINT = 'dewPoint'
+COLUMN_PRECIP_PROBABILITY = 'precipProbability'
+COLUMN_DISHWASHER = 'Dishwasher [kW]'
+COLUMN_FURNACE_1 = 'Furnace 1 [kW]'
+COLUMN_FURNACE_2 = 'Furnace 2 [kW]'
+COLUMN_FRIDGE = 'Fridge [kW]'
+COLUMN_WELL = 'Well [kW]'
+COLUMN_MICROWAVE = 'Microwave [kW]'
+COLUMN_APPARENT_TEMPERATURE = 'apparentTemperature'
+COLUMN_USE = 'use [kW]'
+COLUMN_GEN = 'gen [kW]'
+COLUMN_HOUSE_OVERALL = 'House overall [kW]'
+COLUMN_HOME_OFFICE = 'Home office [kW]'
+COLUMN_LIVING_ROOM = 'Living room [kW]'
+COLUMN_KITCHEN_12 = 'Kitchen 12 [kW]'
+COLUMN_KITCHEN_14 = 'Kitchen 14 [kW]'
+COLUMN_KITCHEN_38 = 'Kitchen 38 [kW]'
+COLUMN_WINE_CELLAR = 'Wine cellar [kW]'
+COLUMN_GARAGE_DOOR = 'Garage door [kW]'
+COLUMN_BARN = 'Barn [kW]'
+COLUMN_TIME = 'time'
+# -------------------------------------------------
